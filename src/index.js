8029b5b8cefa95dc4202e64bede71c6095ff3e27
nuitfsdev <20521631@gm.uit.edu.vn>
2022-11-24 13:26:53 +0700

    add cors

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 const express = require('express')
 require('./db/mongoose')
+const cors=require('cors')
 const carRouter=require('./routers/car')
 const app = express()
 
 const port=process.env.PORT || 3000
+app.use(cors())
 app.use(express.json())
 app.use(carRouter)
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.listen(port, ()=>{
   console.log('Server is up on PORT '+port)
 })
